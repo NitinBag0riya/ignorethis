e1f4ad4ad5d1f837f90e2c06de01298bb1987988
Nitin Bagoriya <nitin.bagoriya@hippocabs.com>
2020-04-29 14:27:54 +0530

    stateProps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Post } from './components/Post'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      post : []
+    }
+  }
+
+  //api data -> api call -> state save 
+  /// mount 
+
+  //fetch , array iteration methods 
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(data => this.setState({
+      post : data
+    }))
+  }
+
+  render(){
+    return(
+      <div>        
+        <Post users={this.state.post} />
+      </div>
+    )
+  }
+
 }
 
-export default App;
